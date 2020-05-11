bd1b646171c2487dfe153e9e283e5becf0b25d6b
lscena <54162064+lscena@users.noreply.github.com>
2020-05-11 15:14:13 -0300

    post clase 7

--- app/src/main/java/com/example/convertidoruniversal/CocinaActivity.java
+++ app/src/main/java/com/example/convertidoruniversal/CocinaActivity.java
@@ -3,6 +3,11 @@ package com.example.convertidoruniversal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CocinaActivity extends AppCompatActivity {
 
@@ -10,5 +15,24 @@ public class CocinaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cocina);
+
+        //El listview se carga dinámicamente desde código.
+        ListView cocinaListView=findViewById(R.id.cocinaListView);
+
+        //creo una lista
+        List<String> listCocina = new ArrayList<String>();
+        //asigno elementos a la lista
+        listCocina.add("azucar");
+        listCocina.add("harina");
+
+        //creo un adaptador
+        ArrayAdapter<String> adaptador = new ArrayAdapter<String>(
+                getApplicationContext(),
+                android.R.layout.simple_list_item_1,
+                listCocina
+        );
+
+        //asigno el adaptador
+        cocinaListView.setAdapter(adaptador);
     }
 }
--- app/src/main/java/com/example/convertidoruniversal/PresionActivity.java
+++ app/src/main/java/com/example/convertidoruniversal/PresionActivity.java
@@ -3,12 +3,69 @@ package com.example.convertidoruniversal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PresionActivity extends AppCompatActivity {
+
+    //Variables globales:
+    Spinner fromUnitSpinner;
+    Spinner toUnitSpinner;
+    Float intermediateValue;
+    Float resultValue;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_presion);
+
+        //Creo una lista con los elementos que usará el spinner:
+        //El tipado "E" dentro del list es para indicar que no está definido, pero en este caso uso "String"
+        List<String> listUnits = new ArrayList<>();
+        //Agrego valores a la lista:
+        listUnits.add("Pa");
+        listUnits.add("bar");
+        listUnits.add("kPa");
+        listUnits.add("MPa");
+        listUnits.add("PSI");
+        listUnits.add("atm");
+        listUnits.add("mmHg");
+        listUnits.add("kg/cm²");
+
+        //Creo un adaptador (recibe string y devuelve elementos para el spinner)
+        ArrayAdapter<String> adaptadorPresion= new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_item,listUnits);
+        //Parametros: context, formato, lista que quiero usar
+        //Nota: el formato simple_spinner_dropdown_item son items más amplios visualmente.
+
+        //pongo los spinner en variable
+        fromUnitSpinner=findViewById(R.id.fromUnitSpinner);
+        toUnitSpinner=findViewById(R.id.toUnitSpinner);
+
+        //Le seteo el adaptador al spinner:
+        fromUnitSpinner.setAdapter(adaptadorPresion);
+        toUnitSpinner.setAdapter(adaptadorPresion);
+    }
+
+    public void convertirPresion(View view) {
+        String selectionFrom = fromUnitSpinner.getSelectedItem().toString();
+        String selectionTo = fromUnitSpinner.getSelectedItem().toString();
+
+        if(selectionFrom.isEmpty() || selectionTo.isEmpty()){
+            Toast.makeText(getApplicationContext(),"Ingrese una presión.",Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        switch(selectionFrom){
+            case("Pa"):
+                intermediateValue=2f; //poner la formula
+
+        }
+
     }
 }
